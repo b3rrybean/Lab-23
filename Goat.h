bb8c75aa8f94a98cf44caeb228f5e5582fb21884
Nicole C. <nmcanson@gmail.com>
2025-10-25 13:21:59 -0700

    update Goat.h?

--- Goat.h
+++ Goat.h
@@ -13,9 +13,23 @@ private:
 public: 
     Goat()                          { name = ""; age = 0; color = ""; }
     // write three more constructors
-    
+    Goat(string n) {
+        name = n;
+        age = 0;
+        color = "";
+    }
 
+    Goat(string n, int a) {
+        name = n;
+        age = a;
+        color = "";
+    }
 
+    Goat(string n, int a, string c) {
+        name = n;
+        age = a;
+        color = c;
+    }
 
     // setters and getters
     void set_name(string n)         { name = n; };
@@ -26,6 +40,9 @@ public:
     string get_color() const        { return color; }
 
     // write overloaded < operator for the std::list
+    bool operator<(const Goat &other) const {
+        return name < other.name;
+    }
 };
 
 #endif
